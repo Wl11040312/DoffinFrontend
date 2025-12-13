9b7567208a108a7d170fbc723918863b325e26cf
Wl11040312 <weilun11040312@gmail.com>
2025-12-13 12:10:27 +0800

    Setup PrimeNG

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  standalone: false,
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule],
   styleUrl: './app.css'
 })
 export class App {
